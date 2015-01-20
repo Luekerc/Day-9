4dd5f2fd995bffa2b28a16fda2b970588879b40f
Charles Lueker <charleslueker@yahoo.com>
2015-01-19 19:34:06 -0600

    Assignments 1 and the start of 2

---
+++ Assignment2/main.js
@@ -0,0 +1,44 @@
+var myArray=["item A","item B","item C"];
+
+var number=(Math.floor(Math.random()*3));
+
+console.log(number);
+
+
+if (number===0) {
+	console.log(myArray[0]);
+}
+
+else if (number===1){
+	console.log(myArray[1]);
+}
+
+else {
+	console.log(myArray[2]);
+};
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
