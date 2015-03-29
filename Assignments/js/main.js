881f06f13b19e1fa95b25006ee37890647a72be2
Charles Lueker <charleslueker@yahoo.com>
2015-03-28 21:22:18 -0500

    still needs some validation cleaning up

--- Assignments/js/main.js
+++ Assignments/js/main.js
@@ -192,6 +192,132 @@ $('#btn4').click(function() {
 });
 
 
+//Function 5
+function randomStudent (studentArray) {
+
+	//variable declaration
+
+	var resultArray = [];
+	var randomNum = 0;
+	var length = studentArray.length;
+
+	//data validation
+
+	if (typeof studentArray != 'object') {
+		throw 'You did not enter an array.';
+	}
+
+
+	//function 
+	for(var i = 0; i <= length; i++) {
+
+		var randomPair = [];
+		randomNum = Math.floor(Math.random() * studentArray.length);
+
+		// while (randomNum == (studentArray.length)-1 && studentArray.length != 1) {	
+		// 	randomNum = Math.floor(Math.random() * studentArray.length);
+		// }
+		randomPair.push('[');
+		randomPair.push(studentArray.splice(randomNum, 1));
+		randomNum = Math.floor(Math.random() * studentArray.length);
+		randomPair.push(studentArray.splice(randomNum, 1));
+		randomPair.push(']');
+		resultArray[i] = randomPair;
+	
+	}
+
+	return resultArray;
+
+}
+
+$('#f5').click(function() {
+	var textInput = $('#enter5').val();
+	textArray = textInput.split(',');
+	$('#result5').val(randomStudent(textArray));
+});
+
+//Function 6
+function dashString(string) {
+
+	//variable declaration
+
+	var arrayOfString = string.split('');
+
+	//data validation
+
+	if (typeof string != 'string') {
+		throw 'You did not enter an string.';
+	}
+
+	//function 
+
+	return arrayOfString.join('-');
+
+}
+
+
+$('#f6').click(function() {
+	var textInput = $('#enter6').val();
+	$('#result6').val(dashString(textInput));
+});
+//Function 7
+function noJoinString(string) {
+
+	//variable declaration
+
+	var newString = '';
+
+	//data validation
+
+	if (typeof string != 'string') {
+		throw 'You did not enter an string.';
+	}
+
+	//function
+
+	for(var i =0; i < string.length; i++) {
+        newString += string.slice(i, i+1)+"-";
+	}
+	newString = newString.slice(0,-1);
+	return newString;
+}
+
+$('#f7').click(function() {
+	var textInput = $('#enter7').val();
+	$('#result7').val(dashString(textInput));
+});
+
+//Function 8
+function noJoin2Strings(string1, string2) {
+
+	//variable declaration
+
+	var combinedString = string1 + string2;
+	console.log(combinedString);
+	var newString = '';
+
+	//data validation
+
+	if (typeof string1 !='string' || typeof string2 !='string') {
+
+	}
+
+
+	//function
+
+	for(var i =0; i < combinedString.length; i++) {
+        newString += combinedString.slice(i, i+1)+"-";
+	}
+	newString = newString.slice(0,-1);
+	return newString;
+}
+
+$('#f8').click(function() {
+	var textInput = $('#enter8').val();
+	var textInput2 = $('#enter8-1').val();
+
+	$('#result8').val(noJoin2Strings(textInput,textInput2));
+});
 })
 
 
